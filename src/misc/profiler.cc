fe40269a72c95bd577b574c8a7281c1d57496f52
Greg Inozemtsev <greg@enfabrica.net>
2023-09-05 11:39:59 -0700

    Convert USDT probe to single struct value

--- src/misc/profiler.cc
+++ src/misc/profiler.cc
@@ -1,25 +1,40 @@
 #include "profiler.h"
 
 #include <sys/sdt.h>
-#define USDT_PROBE12(name, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12) \
-	DTRACE_PROBE12(nccl, name, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12)
+#define USDT_PROBE1(name, a1) \
+	DTRACE_PROBE1(nccl, name, a1)
+
+struct nccl_trace_event {
+	uint64_t comm;
+	uint64_t opCount;
+	uint8_t state;
+	uint8_t coll;
+	uint8_t step;
+	uint8_t channelId;
+	uint16_t sliceSteps;
+	uint16_t chunkSteps;
+	uint16_t chunkSize;
+	uint16_t nsteps;
+	uint32_t nbytes;
+	int peer;
+};
 
 ncclResult_t ncclProfilingRecord(struct ncclProxyArgs* args, int sub, int step, enum ncclProxyProfileState state) {
-  if (state <= ncclProxyProfileSendEnd) {
-  //if (state <= ncclProxyProfileRecvEnd) {
-    USDT_PROBE12(sendrecv,
-      state,
-      args->comm,
-      args->coll - NCCL_NUM_FUNCTIONS,
-      args->opCount,
-      args->subs[sub].peer,
-      step,
-      args->subs[sub].channelId,
-      args->sliceSteps,
-      args->chunkSteps,
-      args->chunkSize,
-      args->subs[sub].nsteps,
-      args->subs[sub].nbytes);
+  struct nccl_trace_event e = {};
+  if (state <= ncclProxyProfileRecvEnd) {
+    e.state = state;
+    e.comm = args->comm;
+    e.coll = args->coll - NCCL_NUM_FUNCTIONS;
+    e.opCount = args->opCount;
+    e.peer = args->subs[sub].peer;
+    e.step = step;
+    e.channelId = args->subs[sub].channelId;
+    e.sliceSteps = args->sliceSteps;
+    e.chunkSteps = args->chunkSteps;
+    e.chunkSize = args->chunkSize;
+    e.nsteps = args->subs[sub].nsteps;
+    e.nbytes = args->subs[sub].nbytes;
+    USDT_PROBE1(sendrecv, &e);
   }
   return ncclSuccess;
 }
